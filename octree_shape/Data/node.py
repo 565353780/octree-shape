49ca18a1a5cf6a1f93bb924c192ce2c3683f518d
chLi <565353780@qq.com>
2025-08-17 13:31:31 +0800

    add toAABB algo

--- octree_shape/Data/node.py
+++ octree_shape/Data/node.py
@@ -1,10 +1,11 @@
 import numpy as np
+from typing import Tuple
 
 
 class Node:
     def __init__(
         self,
-        id: str = "0",
+        id: str = "",
         child_state: np.uint8 = np.uint8(0),
     ):
         self.id = id
@@ -49,8 +50,31 @@ class Node:
                 child_idxs.append(i)
         return np.asarray(child_idxs)
 
-    def toAABB(self) -> np.ndarray:
-        return
+    def toAABB(self, scale: float = 1.0) -> Tuple[np.ndarray, np.ndarray]:
+        half_length = scale / 2.0
+        aabb_min = np.array(
+            [-half_length, -half_length, -half_length], dtype=np.float64
+        )
+        aabb_max = np.array([half_length, half_length, half_length], dtype=np.float64)
+
+        for id_str in self.id:
+            current_half_length = (aabb_max - aabb_min) / 2.0
+
+            if id_str in "0246":
+                aabb_max[0] -= current_half_length[0]
+            else:
+                aabb_min[0] += current_half_length[0]
+
+            if id_str in "0145":
+                aabb_max[1] -= current_half_length[1]
+            else:
+                aabb_min[1] += current_half_length[1]
+
+            if id_str in "0123":
+                aabb_max[2] -= current_half_length[2]
+            else:
+                aabb_min[2] += current_half_length[2]
+        return aabb_min, aabb_max
 
     def addChild(self, child_idx: int):
         if not self.updateChildState(child_idx, True):
--- octree_shape/Demo/node.py
+++ octree_shape/Demo/node.py
@@ -2,13 +2,17 @@ from octree_shape.Data.node import Node
 
 
 def demo():
-    root = Node()
-
-    root.child_state = 195  # 0b11000011
+    root = Node("07", 195)  # 0b11000011
 
     print("child_state:", root.child_state)
 
     child_idxs = root.toChildIdxs()
 
     print("child_idxs:", child_idxs)
+
+    aabb_min, aabb_max = root.toAABB()
+
+    print("aabb:")
+    print(aabb_min)
+    print(aabb_max)
     return True
