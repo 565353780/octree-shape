218d0860e2a0d5bc750d468a2378b3c4d5f291a5
chLi <565353780@qq.com>
2025-09-10 21:02:54 +0800

    create test speed func

--- octree_shape/Demo/occ_sampler.py
+++ octree_shape/Demo/occ_sampler.py
@@ -4,21 +4,32 @@ from tqdm import trange
 from octree_shape.Module.occ_sampler import OccSampler
 
 
+def test_speed(
+    mesh_file_path: str,
+    subdiv_depth: int,
+    occ_depth: int,
+):
+    output_info = False
+
+    print("start test speed of queryOrderedOcc...")
+    for i in trange(100):
+        occ_sampler = OccSampler(mesh_file_path, subdiv_depth, occ_depth, output_info)
+        occ = occ_sampler.queryOrderedOcc(i)
+    return True
+
+
 def demo():
     home = os.environ["HOME"]
 
     mesh_file_path = home + "/chLi/Dataset/Famous/bunny-v2.ply"
     # mesh_file_path = home + "/chLi/Dataset/vae-eval/mesh/002.obj"
     subdiv_depth = 3
-    occ_depth = 7
+    occ_depth = 6
     output_info = True
 
-    occ_sampler = OccSampler(mesh_file_path, subdiv_depth, occ_depth, output_info)
+    test_speed(mesh_file_path, subdiv_depth, occ_depth)
 
-    print("start test speed of queryOrderedOcc...")
-    for i in trange(occ_sampler.subdiv_centers.shape[0]):
-        occ = occ_sampler.queryOrderedOcc(i)
-    exit()
+    occ_sampler = OccSampler(mesh_file_path, subdiv_depth, occ_depth, output_info)
 
     print("start renderMergeOcc...")
     occ_sampler.renderMergeOcc()
